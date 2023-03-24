9f53b459db606b679391775134e6fb884fa70f87
Alexander Melnyk <amelnyk@amazon.com>
2023-03-24 14:36:21 +0100

    init

--- src/main/java/com/myorg/HelloWorldStack.java
+++ src/main/java/com/myorg/HelloWorldStack.java
@@ -1,10 +1,22 @@
 package com.myorg;
 
+import software.amazon.awscdk.*;
+import software.amazon.awscdk.services.apigatewayv2.alpha.*;
+import software.amazon.awscdk.services.apigatewayv2.integrations.alpha.HttpLambdaIntegration;
+import software.amazon.awscdk.services.apigatewayv2.integrations.alpha.HttpLambdaIntegrationProps;
+import software.amazon.awscdk.services.lambda.Code;
+import software.amazon.awscdk.services.lambda.Function;
+import software.amazon.awscdk.services.lambda.FunctionProps;
+import software.amazon.awscdk.services.lambda.Runtime;
+import software.amazon.awscdk.services.logs.RetentionDays;
+import software.amazon.awscdk.services.s3.assets.AssetOptions;
 import software.constructs.Construct;
-import software.amazon.awscdk.Stack;
-import software.amazon.awscdk.StackProps;
-// import software.amazon.awscdk.Duration;
-// import software.amazon.awscdk.services.sqs.Queue;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static java.util.Collections.singletonList;
+import static software.amazon.awscdk.BundlingOutput.ARCHIVED;
 
 public class HelloWorldStack extends Stack {
     public HelloWorldStack(final Construct scope, final String id) {
@@ -14,11 +26,54 @@ public class HelloWorldStack extends Stack {
     public HelloWorldStack(final Construct scope, final String id, final StackProps props) {
         super(scope, id, props);
 
-        // The code that defines your stack goes here
+        List<String> functionPackagingInstructions = Arrays.asList(
+                "/bin/sh",
+                "-c",
+                "cd Function " +
+                        "&& mvn clean install " +
+                        "&& cp /asset-input/Function/target/Function-1.0-SNAPSHOT.jar /asset-output/"
+        );
+        BundlingOptions.Builder builderOptions = BundlingOptions.builder()
+                .command(functionPackagingInstructions)
+                .image(Runtime.JAVA_11.getBundlingImage())
+                .volumes(singletonList(
+                        // Mount local .m2 repo to avoid download all the dependencies again inside the container
+                        DockerVolume.builder()
+                                .hostPath(System.getProperty("user.home") + "/.m2/")
+                                .containerPath("/root/.m2/")
+                                .build()
+                ))
+                .user("root")
+                .outputType(ARCHIVED);
 
-        // example resource
-        // final Queue queue = Queue.Builder.create(this, "HelloWorldQueue")
-        //         .visibilityTimeout(Duration.seconds(300))
-        //         .build();
+        Function function = new Function(this, "Function", FunctionProps.builder()
+                .runtime(Runtime.JAVA_11)
+                .code(Code.fromAsset("app", AssetOptions.builder()
+                        .bundling(builderOptions
+                                .command(functionPackagingInstructions)
+                                .build())
+                        .build()))
+                .handler("helloworld.App")
+                .memorySize(1024)
+                .timeout(Duration.seconds(10))
+                .logRetention(RetentionDays.ONE_WEEK)
+                .build());
+
+        HttpApi httpApi = new HttpApi(this, "sample-api", HttpApiProps.builder()
+                .apiName("sample-api")
+                .build());
+
+        httpApi.addRoutes(AddRoutesOptions.builder()
+                .path("/")
+                .methods(singletonList(HttpMethod.GET))
+                .integration(new HttpLambdaIntegration("function", function, HttpLambdaIntegrationProps.builder()
+                        .payloadFormatVersion(PayloadFormatVersion.VERSION_2_0)
+                        .build()))
+                .build());
+
+        new CfnOutput(this, "HttpApi", CfnOutputProps.builder()
+                .description("Url for Http Api")
+                .value(httpApi.getApiEndpoint())
+                .build());
     }
 }
